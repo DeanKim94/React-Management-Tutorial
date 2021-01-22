98d08c9b310697dc6f24c4671019bd41bad5d17d
jptr94 <jptr94@gmail.com>
2021-01-22 15:43:09 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
+import Customer from './components/Customer.js'
 import './App.css';
+
+const customers = [ {
+  'id': 1,
+  'image':'https://placeimg.com/64/64/1',
+  'name': '홍길숙',
+  'birthday': '951222',
+  'gender': '여자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image':'https://placeimg.com/64/64/2',
+  'name': '홍숙',
+  'birthday': '951222',
+  'gender': 'boy',
+  'job': '선생님'
+},
+{
+  'id': 3,
+  'image':'https://placeimg.com/64/64/3',
+  'name': '기길숙',
+  'birthday': '941222',
+  'gender': 'girl',
+  'job': 'programmer'
+}
+]
 
 function App() {
   return (
-    <div className="gray-background">
-       <img src={logo} alt="logo" />
-       <h2> React Project Tutorial</h2>   
-    </div>
-  );
+  <div>
+    {
+      customers.map(c=>{
+        return(
+        <Customer 
+            id = {c.id}
+            image ={c.image}
+            name = {c.name}
+            birthday={c.birthday}
+            gender = {c.gender}
+            job = {c.job}
+        />
+        )
+        }
+      )
+    }
+  </div>
+  ); 
 }
 
 export default App;
